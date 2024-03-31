7ec9030f1187bbc51eceeaf954ecb4a680e3477f
Michael Gift <simiyugift@outlook.com>
2024-03-31 05:04:26 +0300

    Chore(Dependencies): Bump target and compile sdk from 33 to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.myth.journi"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.myth.journi"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
