b31ef49a3163ae8a8c1afbd02e0b383f04f0915d
Michael Gift <simiyugift@outlook.com>
2024-04-06 16:16:50 +0300

    Add floating action bar

--- app/src/main/java/com/myth/journi/presentation/screens/task/TaskListScreen.kt
+++ app/src/main/java/com/myth/journi/presentation/screens/task/TaskListScreen.kt
@@ -6,7 +6,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddTask
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -38,6 +42,14 @@ fun TaskListScreen() {
                 onNavigationClick = { /*TODO*/ },
                 streak = "1"
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Default.AddTask,
+                    contentDescription = "Add Task"
+                )
+            }
         }
     ) { padding ->
         Column(
