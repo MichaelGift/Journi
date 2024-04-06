25cf0d205c78a5efe5e4e8902ece0df5c7ad95e6
Michael Gift <simiyugift@outlook.com>
2024-04-06 22:00:55 +0300

    Rename Actions to Action

--- app/src/main/java/com/myth/journi/data/database/ActionDao.kt
+++ app/src/main/java/com/myth/journi/data/database/ActionDao.kt
@@ -6,22 +6,22 @@ import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import com.myth.journi.domain.model.ACTIONS_TABLE
-import com.myth.journi.domain.model.Actions
+import com.myth.journi.domain.model.Action
 
 @Dao
 interface ActionDao {
     @Insert
-    suspend fun addAction(action: Actions)
+    suspend fun addAction(action: Action)
 
     @Update
-    suspend fun updateAction(action: Actions)
+    suspend fun updateAction(action: Action)
 
     @Delete
-    suspend fun deleteAction(actions: Actions)
+    suspend fun deleteAction(action: Action)
 
     @Query("SELECT * FROM $ACTIONS_TABLE")
-    suspend fun getAllActions(): List<Actions>
+    suspend fun getAllActions(): List<Action>
 
     @Query("SELECT * FROM $ACTIONS_TABLE WHERE startDate >= :today AND endDate <= :today")
-    suspend fun getTodaysActions(today: Long): List<Actions>
+    suspend fun getTodaysActions(today: Long): List<Action>
 }
