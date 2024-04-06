864621d06ab1338bb80f0c054a4c747bff4d97a2
Michael Gift <simiyugift@outlook.com>
2024-04-06 22:01:37 +0300

    Change abstract val to fun

--- app/src/main/java/com/myth/journi/data/database/JourniDB.kt
+++ app/src/main/java/com/myth/journi/data/database/JourniDB.kt
@@ -3,7 +3,7 @@ package com.myth.journi.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.myth.journi.domain.model.Actions
+import com.myth.journi.domain.model.Action
 import com.myth.journi.domain.model.Goal
 import com.myth.journi.domain.model.Pomodoro
 import com.myth.journi.domain.model.Task
@@ -15,7 +15,7 @@ import com.myth.journi.domain.model.converters.TaskConverter
 @Database(
     entities = [
         Goal::class,
-        Actions::class,
+        Action::class,
         Pomodoro::class,
         Task::class
     ],
@@ -28,8 +28,8 @@ import com.myth.journi.domain.model.converters.TaskConverter
     TaskConverter::class
 )
 abstract class JourniDB : RoomDatabase() {
-    abstract val goalDao: GoalDao
-    abstract val actionDao: ActionDao
-    abstract val pomodoroDao: PomodoroDao
-    abstract val taskDao: TaskDao
+    abstract fun goalDao(): GoalDao
+    abstract fun actionDao(): ActionDao
+    abstract fun pomodoroDao(): PomodoroDao
+    abstract fun taskDao(): TaskDao
 }
