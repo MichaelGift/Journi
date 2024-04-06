b9adaaae421403a14776f5345bf7587b4b0bb793
Michael Gift <simiyugift@outlook.com>
2024-04-06 22:06:01 +0300

    Add Goal repo interface and implementation

---
+++ app/src/main/java/com/myth/journi/data/database/GoalRepoImpl.kt
@@ -0,0 +1,30 @@
+package com.myth.journi.data.database
+
+import com.myth.journi.domain.model.Goal
+import com.myth.journi.domain.repository.GoalRepo
+import javax.inject.Inject
+
+class GoalRepoImpl @Inject constructor(
+    private val goalDao: GoalDao
+) : GoalRepo {
+    override suspend fun saveGoal(goal: Goal) {
+        goalDao.saveGoal(goal)
+    }
+
+    override suspend fun updateGoal(goal: Goal) {
+        goalDao.updateGoal(goal)
+    }
+
+    override suspend fun deleteGoal(goal: Goal) {
+        goalDao.deleteGoal(goal)
+    }
+
+    override suspend fun deleteGoals(goals: List<Goal>) {
+        goalDao.deleteGoals(goals)
+    }
+
+    override suspend fun getAllGaols(): List<Goal> {
+        return goalDao.getAllGoals()
+    }
+
+}
