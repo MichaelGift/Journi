5aeb22691edadbea757ba8edf8d47545f2725ad1
Michael Gift <simiyugift@outlook.com>
2024-04-06 19:50:48 +0300

    Stabilize hilt and compose versions with kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.11"
     }
     packaging {
         resources {
@@ -53,7 +53,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -85,8 +85,8 @@ dependencies {
     testImplementation("androidx.room:room-testing:$roomVersion")
 
     // Hilt for DI
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
 
     // Gson converter
     implementation("com.google.code.gson:gson:2.10.1")
