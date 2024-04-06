dcfba2291b6700ac22d8f7b4978dd53f32ef0435
Michael Gift <simiyugift@outlook.com>
2024-04-06 19:36:07 +0300

    Update converter to converter task list instead

--- app/src/main/java/com/myth/journi/domain/model/converters/TaskConverter.kt
+++ app/src/main/java/com/myth/journi/domain/model/converters/TaskConverter.kt
@@ -2,16 +2,30 @@ package com.myth.journi.domain.model.converters
 
 import androidx.room.TypeConverter
 import com.google.gson.Gson
+import com.google.gson.JsonArray
+import com.google.gson.JsonParser
 import com.myth.journi.domain.model.Task
 
 class TaskConverter {
+    private val gson = Gson()
+
     @TypeConverter
-    fun fromTask(task: Task): String {
-        return Gson().toJson(task)
+    fun fromTask(task: List<Task>): String {
+        val jsonArray = JsonArray()
+        task.forEach {
+            val jsonObject = gson.toJsonTree(it).asJsonObject
+            jsonArray.add(jsonObject)
+        }
+        return jsonArray.toString()
     }
 
     @TypeConverter
-    fun toTask(task: String): Task {
-        return Gson().fromJson(task, Task::class.java)
+    fun toTask(task: String): List<Task> {
+        val jsonArray = JsonParser.parseString(task).asJsonArray
+        val taskList = mutableListOf<Task>()
+        jsonArray.forEach { jsonObject ->
+            taskList.add(gson.fromJson(jsonObject, Task::class.java))
+        }
+        return taskList
     }
 }
