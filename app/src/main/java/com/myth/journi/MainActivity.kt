cf9d61d62a87e37185d79ef137e4b4f382a22d27
Michael Gift <simiyugift@outlook.com>
2024-04-06 16:07:58 +0300

    Configure app to book to task list screen

--- app/src/main/java/com/myth/journi/MainActivity.kt
+++ app/src/main/java/com/myth/journi/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.myth.journi.presentation.screens.task.TaskListScreen
 import com.myth.journi.ui.theme.JourniTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +23,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TaskListScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JourniTheme {
-        Greeting("Android")
     }
 }
