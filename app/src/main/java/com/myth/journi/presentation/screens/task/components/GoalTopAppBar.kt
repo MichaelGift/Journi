bdad9ceefb4e4d686a579cbd16e502a037a8c823
Michael Gift <simiyugift@outlook.com>
2024-04-07 18:44:27 +0300

    Components tasklist screen

--- app/src/main/java/com/myth/journi/presentation/screens/task/components/GoalTopAppBar.kt
+++ app/src/main/java/com/myth/journi/presentation/screens/task/components/GoalTopAppBar.kt
@@ -2,6 +2,7 @@ package com.myth.journi.presentation.screens.task.components
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Save
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -13,11 +14,19 @@ import androidx.compose.runtime.Composable
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GoalTopAppBar(
-    title: String, onNavigationClick: () -> Unit
+    title: String, onNavigationClick: () -> Unit, onSaveIconClick: () -> Unit
 ) {
-    TopAppBar(title = { Text(text = title) }, navigationIcon = {
-        IconButton(onClick = onNavigationClick) {
-            Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+    TopAppBar(
+        title = { Text(text = title) },
+        navigationIcon = {
+            IconButton(onClick = onNavigationClick) {
+                Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+            }
+        },
+        actions = {
+            IconButton(onClick = onSaveIconClick) {
+                Icon(Icons.Filled.Save, contentDescription = "Save")
+            }
         }
-    })
+    )
 }
---
+++ app/src/main/java/com/myth/journi/presentation/screens/task/components/TaskCard.kt
@@ -0,0 +1,170 @@
+package com.myth.journi.presentation.screens.task.components
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Category
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProgressIndicatorDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.myth.journi.domain.model.Goal
+
+@Composable
+fun TaskCard(
+    item: Goal,
+    modifier: Modifier = Modifier
+) {
+    ElevatedCard(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(all = 8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    TaskTimeStart(item.actionSteps.timeBlockStart.toString())
+                    TaskTimeEnd(item.actionSteps.timeBlockEnd.toString())
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Separator()
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Column(modifier = Modifier.weight(1f)) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        TaskCategoryIcon()
+                        Spacer(modifier = Modifier.width(4.dp))
+                        TaskCategoryTitle(item.category)
+                        Spacer(modifier = Modifier.width(4.dp))
+                    }
+
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        TaskName(item.title)
+                    }
+                }
+
+                Spacer(modifier = Modifier.width(8.dp))
+                Separator()
+                Spacer(modifier = Modifier.width(8.dp))
+
+                TaskProgressIndicator(item.actionSteps.completed, item.actionSteps.total)
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskDueDate(date: String) {
+    Text(text = date, style = MaterialTheme.typography.bodySmall)
+}
+
+@Composable
+fun Separator() {
+    Box(
+        modifier = Modifier
+            .height(40.dp)
+            .width(2.dp)
+            .background(Color.Gray)
+    )
+}
+
+@Composable
+fun TaskName(name: String) {
+    Text(text = name, style = MaterialTheme.typography.titleMedium)
+}
+
+@Composable
+fun TaskProgressIndicator(completed: Int, total: Int) {
+    val progress = getPercentage(completed, total)
+    val animatedProgress by animateFloatAsState(
+        targetValue = progress,
+        animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec,
+        label = ""
+    )
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(40.dp)
+    ) {
+        CircularProgressIndicator(
+            progress = animatedProgress,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.size(40.dp)
+        )
+        Text(
+            text = "${(progress * 100).toInt()}%",
+            style = MaterialTheme.typography.bodySmall.copy(
+                fontWeight = FontWeight.Bold
+            )
+        )
+    }
+}
+
+fun getPercentage(completed: Int, total: Int): Float {
+    return (completed.toFloat() / total.toFloat())
+}
+
+@Composable
+fun TaskLevel(current: Int, max: Int) {
+    Text(text = "Lv $current / $max", style = MaterialTheme.typography.bodySmall)
+}
+
+@Composable
+fun TaskCategoryTitle(category: String) {
+    Text(text = category, style = MaterialTheme.typography.bodySmall)
+}
+
+@Composable
+fun TaskCategoryIcon() {
+    Icon(
+        imageVector = Icons.Default.Category,
+        contentDescription = "Task Icon",
+        modifier = Modifier.size(16.dp)
+    )
+}
+
+@Composable
+fun TaskTimeEnd(text: String) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.bodySmall
+    )
+}
+
+@Composable
+fun TaskTimeStart(text: String) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.titleMedium.copy(
+            fontWeight = FontWeight.Bold
+        ),
+        modifier = Modifier.padding(
+            bottom = 4.dp
+        )
+    )
+}
