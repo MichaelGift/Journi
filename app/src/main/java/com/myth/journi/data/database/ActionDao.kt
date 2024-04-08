15174d10bc201c6eaa9fe46cf35d838c7e6c7bea
Michael Gift <simiyugift@outlook.com>
2024-04-08 05:55:51 +0300

    Add get by id query
    Change return type to Flow<>

--- app/src/main/java/com/myth/journi/data/database/ActionDao.kt
+++ app/src/main/java/com/myth/journi/data/database/ActionDao.kt
@@ -3,15 +3,17 @@ package com.myth.journi.data.database
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.myth.journi.domain.model.ACTIONS_TABLE
 import com.myth.journi.domain.model.Action
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ActionDao {
-    @Insert
-    suspend fun addAction(action: Action)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addAction(action: Action): Long
 
     @Update
     suspend fun updateAction(action: Action)
@@ -20,8 +22,11 @@ interface ActionDao {
     suspend fun deleteAction(action: Action)
 
     @Query("SELECT * FROM $ACTIONS_TABLE")
-    suspend fun getAllActions(): List<Action>
+    fun getAllActions(): Flow<List<Action>>
 
     @Query("SELECT * FROM $ACTIONS_TABLE WHERE startDate >= :today AND endDate <= :today")
     suspend fun getTodaysActions(today: Long): List<Action>
+
+    @Query("SELECT * FROM $ACTIONS_TABLE WHERE id = :id")
+    fun getActionById(id: Long): Flow<Action>
 }
