6961c254dec90e180f74d6925e7937060ca2e0fa
Michael Gift <simiyugift@outlook.com>
2024-04-08 05:54:44 +0300

    Update routing naming

--- app/src/main/java/com/myth/journi/presentation/MainActivity.kt
+++ app/src/main/java/com/myth/journi/presentation/MainActivity.kt
@@ -7,12 +7,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.myth.journi.common.utils.Screen
+import com.myth.journi.presentation.screens.pomodoro.PomodoroScreen
 import com.myth.journi.presentation.screens.task.CreateGoalScreen
-import com.myth.journi.presentation.screens.task.TaskListScreen
+import com.myth.journi.presentation.screens.task.ActionsListScreen
 import com.myth.journi.ui.theme.JourniTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,19 +27,28 @@ class MainActivity : ComponentActivity() {
             JourniTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     val navController = rememberNavController()
                     NavHost(
-                        navController = navController,
-                        startDestination = Screen.TaskList.route
+                        navController = navController, startDestination = Screen.ActionsListScreen.route
                     ) {
-                        composable(Screen.TaskList.route) {
-                            TaskListScreen(navController = navController)
+                        composable(Screen.ActionsListScreen.route) {
+                            ActionsListScreen(navController = navController)
                         }
-                        composable(Screen.TaskCreation.route) {
+                        composable(Screen.GoalCreationScreen.route) {
                             CreateGoalScreen(navController = navController)
+                        }
+                        composable(
+                            route = Screen.PomodoroScreen.route + "?actionId={actionId}",
+                            arguments = listOf(
+                                navArgument(name = "actionId") {
+                                    type = NavType.LongType
+                                    defaultValue = -1
+                                },
+                            )
+                        ) {
+                            PomodoroScreen(navController = navController)
                         }
                     }
                 }
