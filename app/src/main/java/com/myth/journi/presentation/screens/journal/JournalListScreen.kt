65285dd226f4f9ef97f7074e141ee078bd7619c7
Michael Gift <simiyugift@outlook.com>
2024-05-16 04:32:08 +0300

    Add greetings for user
    #5

--- app/src/main/java/com/myth/journi/presentation/screens/journal/JournalListScreen.kt
+++ app/src/main/java/com/myth/journi/presentation/screens/journal/JournalListScreen.kt
@@ -2,16 +2,14 @@ package com.myth.journi.presentation.screens.journal
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.EditNote
 import androidx.compose.material.icons.filled.LocalFireDepartment
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -26,15 +24,24 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.myth.journi.ui.theme.JourniTheme
+import com.myth.journi.ui.theme.Roboto
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun JournalScreen(){
+fun JournalScreen() {
     val streak by remember { mutableStateOf("10") }
+    val generalTimeOfDay by remember { mutableStateOf("Afternoon") }
+    val name by remember { mutableStateOf("Michael") }
+
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = "Diary") },
+                title = { Text(text = "Diary", fontFamily = Roboto) },
                 navigationIcon = {
                     IconButton(onClick = { /*TODO*/ }) {
                         Icon(imageVector = Icons.Filled.Menu, contentDescription = "App Menu")
@@ -60,8 +67,30 @@ fun JournalScreen(){
     ) { paddingValues ->
         Column(
             modifier = Modifier.padding(paddingValues)
-        ){
+        ) {
+            Text(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                text = "Good $generalTimeOfDay",
+                fontFamily = Roboto,
+            )
 
+
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                text = name,
+                fontFamily = Roboto,
+                fontWeight = FontWeight.Bold,
+                fontSize = 28.sp
+            )
         }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewDiaryList() {
+    JourniTheme {
+        JournalScreen()
     }
 }
