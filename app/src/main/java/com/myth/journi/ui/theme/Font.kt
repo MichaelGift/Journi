d0527123f3317d41e0181b8ded11e5a7f1bf20b4
Michael Gift <simiyugift@outlook.com>
2024-06-03 04:08:22 +0300

    Add roboto font

---
+++ app/src/main/java/com/myth/journi/ui/theme/Font.kt
@@ -0,0 +1,17 @@
+package com.myth.journi.ui.theme
+
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.myth.journi.R
+
+
+val Roboto = FontFamily(
+    Font(R.font.roboto_black, FontWeight.Black),
+    Font(R.font.roboto_bold, FontWeight.Bold),
+    Font(R.font.roboto_light,FontWeight.Light),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_thin, FontWeight.Light)
+)
